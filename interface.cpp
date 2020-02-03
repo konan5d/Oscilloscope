ce6bef406505af984d9979a94be8780e55e41e43
Corentin LEGEAY <corentin.legeay@ynov.com>
2020-02-03 16:41:20 +0100

    Instanciations des widgets et du layout

--- interface.cpp
+++ interface.cpp
@@ -1,15 +1,24 @@
 #include "interface.h"
 #include "ui_interface.h"
+#include <QPainter>
 
 Interface::Interface(QWidget *parent)
     : QWidget(parent)
-    , ui(new Ui::Interface)
 {
-    ui->setupUi(this);
+    setFixedSize(m_largeur, m_hauteur);
 }
 
-Interface::~Interface()
+QPoint Interface::coo(double x, double y)
 {
-    delete ui;
+    return QPoint(int(m_unite*x + m_largeur/2), int(-m_unite*y+m_hauteur/2));
+}
+
+void Interface::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    QPen black(Qt::black,2);
+    painter.setPen(black);
+    painter.drawLine(coo(-5,0), coo(5,0));
+    painter.drawLine(coo(0,5), coo(0,-5));
 }
 
--- interface.h
+++ interface.h
@@ -1,21 +1,20 @@
-#ifndef INTERFACE_H
+﻿#ifndef INTERFACE_H
 #define INTERFACE_H
 
 #include <QWidget>
-
-QT_BEGIN_NAMESPACE
-namespace Ui { class Interface; }
-QT_END_NAMESPACE
+#include <QPoint>
 
 class Interface : public QWidget
 {
     Q_OBJECT
 
+private:
+    int m_largeur=900;
+    int m_hauteur=600;
+    double m_unite=100;
 public:
     Interface(QWidget *parent = nullptr);
-    ~Interface();
-
-private:
-    Ui::Interface *ui;
+    QPoint coo(double x, double y);
+    void paintEvent(QPaintEvent *event) override;
 };
 #endif // INTERFACE_H
